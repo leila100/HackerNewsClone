11283d3b422fa5a3001c2e40d000afef46604e53
Leila <nb.leila10@gmail.com>
2020-09-13 14:04:26 -0500

    Display titles for the first 10 stories

--- components/Main.js
+++ components/Main.js
@@ -2,43 +2,46 @@ import { useState, useEffect } from "react";
 
 import axios from "axios";
 export default function Main() {
-  const [storiesIds, setStoriesIds] = useState([]);
   const [stories, setStories] = useState([]);
 
-  const fetchStoriesIds = async () => {
-    const url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty";
+  const fetchStories = async () => {
+    const baseUrl = "https://hacker-news.firebaseio.com/v0";
     try {
-      const storiesIds = await axios.get(url);
-      setStoriesIds(storiesIds.data);
+      const storiesIds = await axios.get(`${baseUrl}/topstories.json?print=pretty`);
+      const promises = storiesIds.data.slice(0, 10).map(
+        (id) =>
+          new Promise((resolve, reject) => {
+            axios
+              .get(`${baseUrl}/item/${id}.json?print=pretty`)
+              .then((response) => resolve(response.data))
+              .catch((error) => reject(error));
+          })
+      );
+      Promise.all(promises)
+        .then((storiesArr) => setStories(storiesArr))
+        .catch((error) => console.log(error));
     } catch (error) {
-      console.log("problem loading stories id");
+      console.log("problem loading stories");
     }
   };
 
-  const fetchStories = () => {
-    const storiesArr = [];
-    storiesIds.slice(0, 5).forEach((id) => {
-      axios
-        .get(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
-        .then((answer) => {
-          storiesArr.push(answer);
-        })
-        .catch((error) => {
-          console.log("problem getting story with id ", id);
-        });
-    });
-    setStories(storiesArr);
-  };
-
-  useEffect(() => {
-    fetchStoriesIds();
-    fetchStoriesIds();
-  }, []);
-
   useEffect(() => {
     fetchStories();
-  }, [storiesIds]);
+  }, []);
 
   console.log(stories);
-  return <h2>List of stories</h2>;
+  return (
+    <>
+      <h2>List of stories</h2>
+      {stories.length === 0 ? (
+        <h3>No stories</h3>
+      ) : (
+        <ul>
+          {stories.map((story) => (
+            <li key={story.id}>{story.title}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
